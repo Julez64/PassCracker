a24568d28948a9cf5345c33e83f350c1c1f89009
JulienFerluc <ferluc.julien@carrefour.cegepvicto.ca>
2017-03-19 13:06:17 -0400

    Multi-Threading

---
+++ PassCracker/CrackWorker.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PassCracker
+{
+    public class Cracker
+    {
+        private Random random = new Random();
+        private int count = 0;
+        private string password, crack;
+        private int debut, fin, num, i, length, number;
+        private char rand;
+        private bool started;
+        private Thread worker;
+
+        public Cracker(string password,int debut, int fin, int number)
+        {
+            this.password = password;
+            this.debut = debut;
+            this.fin = fin;
+            this.number = number;
+        }
+
+        public void Start()
+        {
+            if (!started){
+                worker = new Thread(cracker);
+                worker.Start();
+                
+            }
+        }
+
+        public void Stop()
+        {
+            worker.Abort();
+        }
+
+        private void cracker()
+        {
+            started = true;
+            do
+            {
+                crack = "";
+                length = 0;
+                i = 0;
+                length = random.Next(debut, fin);
+                while (i != length)
+                {
+                    num = random.Next(0, 26);
+                    rand = (char)('a' + num);
+                    crack = crack.Insert(0, rand.ToString());
+                    i++;
+                }
+                count++;
+            } while (crack != password);
+            started = false;
+            Console.WriteLine("{0} Done",number);
+            worker.Abort();
+        }
+
+        public bool Started
+        {
+            get;
+        }
+    }
+}
---
+++ PassCracker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassCracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Action> workers = new List<Action>();
+            for (int i = 0; i < Environment.ProcessorCount / 2; i++)
+            {
+                Cracker cracker = new Cracker("hello",1,6,i);
+                cracker.Start();
+                workers.Add(delegate { Console.WriteLine(cracker.GetHashCode()); });
+            }
+            foreach (Action action in workers) action();
+            Console.ReadKey();
+        }
+    }
+}
